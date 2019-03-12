a058252fab7a1759d94e43f9d151c34fdfb24c16
y-masutani <masutani@isc.osakac.ac.jp>
2019-03-12 15:33:28 +0900

    キャスタ対応

--- KobukiController/src/test.cpp
+++ KobukiController/src/test.cpp
@@ -15,17 +15,23 @@ void setup()
 {
     vel.v = 0;
     vel.w = 0;
-    setCartVelocity(vel);
 }
 
 //概要：行動決定のための毎回の処理
 //引数：なし
 //戻り値：なし
-void loop()
+Velocity loop(const Orthogonal &pos, const Bumper &bum, int key)
 {
-    int key;
-    getKey(key);
     if (key < 0) {} //何もしない
-    else if (key == 'i') { vel.v += 0.1; } else if (key == ',') { vel.v -= 0.1; } else if (key == 'j') { vel.w += 0.2; } else if (key == 'l') { vel.w -= 0.2; } else if (key == ' ') { vel.v = 0; vel.w = 0; }
-    setCartVelocity(vel);
+    else if (key == '1') { vel.v = 0; vel.w = 0.3; }
+    else if (key == '2') { vel.v = 0; vel.w = 0.6; }
+    else if (key == '3') { vel.v = 0; vel.w = 0.9; }
+    else if (key == '4') { vel.v = 0.1; vel.w = 0.3; }
+    else if (key == '5') { vel.v = 0.1; vel.w = 0.6; }
+    else if (key == '6') { vel.v = 0.1; vel.w = 0.9; }
+    else if (key == '7') { vel.v = 0.2; vel.w = 0.3; }
+    else if (key == '8') { vel.v = 0.2; vel.w = 0.6; }
+    else if (key == '9') { vel.v = 0.2; vel.w = 0.9; }
+    else if (key == ' ') { vel.v = 0; vel.w = 0; }
+    return vel;
 }
